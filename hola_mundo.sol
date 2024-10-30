f360854b87f7659cf68e5ff83a3f7c3137a4d249
HernanAlbertoLaborde <hernanlaborde@gmail.com>
2024-10-29 19:54:08 -0500

    First commit

---
+++ hola_mundo.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24; //definición de versión del compilador, si da error ajustar version en remix
+
+contract hola_mundo {
+    string public saludo = "Saludo 0";
+}
